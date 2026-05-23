6af301c530c237abeec6f82ff588fa0a1881c8c5
azodbooyevozodbek <ozodboyev0609@gmail.com>
2026-05-23 15:18:02 +0500

    hello

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
+TEXT = """🏆Yutuqlar : Yoq
+
+ɪɴꜱᴛᴀɢʀᴀᴍ : Azodboyev.o
+ᴛᴇʟᴇɢʀᴀᴍ : @Azodboyev_o
+ɢɪᴛʜᴜʙ : AzodboyevOzodbek"""
+
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
 	bot.reply_to(message, 'Salom! nima yordam bera olaman?')
@@ -15,4 +21,8 @@ def send_welcome(message):
 def send_welcome(message):
 	bot.reply_to(message, 'Salom! Mening portfolio botimga xush kelibsiz!')
 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text + "\n\n" + TEXT)
+	
 bot.infinity_polling()
