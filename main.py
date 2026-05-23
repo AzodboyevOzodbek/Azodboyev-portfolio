a76c5678be8a1ca0be5960923a9605af284ae7dd
azodbooyevozodbek <ozodboyev0609@gmail.com>
2026-05-23 15:13:09 +0500

    Refactor command handlers for improved clarity and localization

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, 'Salom! nima yordam bera olaman?')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, 'Salom! Mening portfolio botimga xush kelibsiz!')
 
 bot.infinity_polling()
